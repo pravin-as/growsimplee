d32f9e7acac30b414014b2d8406564416e662ed8
Harshith <harshith.jethu@gmail.com>
2023-01-26 13:19:17 +0530

    Rest framework setup

--- growsimplee/growsimplee/urls.py
+++ growsimplee/growsimplee/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
+router = routers.DefaultRouter()
 
-urlpatterns = [
+urlpatterns = router.urls
+
+urlpatterns += [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
